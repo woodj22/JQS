c3259513f7ba737f54067caa048d4d4262477302
woodj22 <joe.wood@bbc.co.uk>
2019-01-06 19:22:19 +0000

    Refactor read messages to read the next queue message then store if not there

--- queue/file_adaptors.py
+++ queue/file_adaptors.py
@@ -2,19 +2,21 @@ import time
 import json
 from .filesystem import FileSystemInterface
 import os
+from .message import make_message
 
 
 class LocalFileSystem(FileSystemInterface):
     def __init__(self, base_file_path):
         self._base_file_path = base_file_path
+        self._queue_position_file_key = os.path.join(self.base_file_path, 'queue_positions.json')
 
-    def store(self, queue_name, content):
+    def store(self, queue_name, content_body):
         key = self.queue_storage_key(queue_name)
 
         with open(key, 'a+') as file:
             byte_position = file.tell()
+            content = make_message(json.dumps(content_body))
             json.dump(content, file)
-            file.write("\n")
             file.write("\n")
             file.close()
 
@@ -25,11 +27,18 @@ class LocalFileSystem(FileSystemInterface):
         with open(key, 'r+') as file:
             file.seek(byte_position)
             line = file.readline()
+
+            if line is '':
+                return '', byte_position
+
+            next_byte_position = file.tell()
+
             file.close()
-        return line
+        return line, next_byte_position
 
     def store_queue_position(self, queue_name, byte_position):
         key = self.queue_position_file_key
+
         with open(key, 'r+') as f:
             data = json.load(f)
             data[queue_name] = byte_position
@@ -37,19 +46,29 @@ class LocalFileSystem(FileSystemInterface):
             json.dump(data, f, indent=4)
             f.truncate()
             f.close()
-        return True
+
+        return byte_position
 
     def read_queue_position(self, queue_name):
         key = self.queue_position_file_key
-        with open(key, 'r+') as f:
+
+        with open(key, 'r') as f:
             data = json.load(f)
+
+            if queue_name not in data:
+                return self.store_queue_position(queue_name, 0)
+
             byte_position = data[queue_name]
 
         return byte_position
 
     @property
     def queue_position_file_key(self):
-        return os.path.join(self.base_file_path, 'queue_positions.json')
+        return self._queue_position_file_key
+
+    @queue_position_file_key.setter
+    def queue_position_file_key(self, value):
+        self._queue_position_file_key = value
 
     def queue_storage_key(self, queue_name):
         return os.path.join(self.base_file_path, queue_name + ".txt")
--- queue/queue.py
+++ queue/queue.py
@@ -9,14 +9,16 @@ class Queue:
     def append_message_to_queue(self, queue_name, message):
         queue_name, byte_position = self.file_adaptor.store(queue_name, message)
 
-        self.file_adaptor.store_queue_position(queue_name, byte_position)
-
         return byte_position
 
     def read_top_message_from_queue(self, queue_name):
-        byte_position = self.file_adaptor.read_queue_position(queue_name)
+        current_byte_position = self.file_adaptor.read_queue_position(queue_name)
 
-        return self.file_adaptor.read(queue_name, byte_position)
+        message, next_byte_position = self.file_adaptor.read(queue_name, current_byte_position)
+
+        self.file_adaptor.store_queue_position(queue_name, next_byte_position)
+
+        return message
 
     @property
     def file_adaptor(self):
